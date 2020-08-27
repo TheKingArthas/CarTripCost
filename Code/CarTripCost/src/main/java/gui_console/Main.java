6120ef8c49448442df6ece7b8974d678196d5488
Federico De Luca <federicoNdeluca@gmail.com>
2020-08-27 12:18:26 -0300

    Car management menu created

--- Code/CarTripCost/src/main/java/gui_console/Main.java
+++ Code/CarTripCost/src/main/java/gui_console/Main.java
@@ -6,12 +6,15 @@
 package gui_console;
 
 import java.util.Scanner;
+import permanence.TempDB;
 
 /**
  *
  * @author Federico De Luca (federicoNdeluca@gmail.com)
  */
 public class Main {
+
+    private static TempDB tempDB;
 
     private static void cleanScreen() {
         for (int i = 0; i < 50; i++) {
@@ -36,9 +39,9 @@ public class Main {
 
     private static void mainMenu() {
         boolean exitProgram = false;
-        int selection;
 
         while (!exitProgram) {
+            cleanScreen();
             System.out.println("<<<CAR-TRIP-PRICE>>>");
             space();
             System.out.println("1) New travel");
@@ -49,9 +52,11 @@ public class Main {
             System.out.println("0) Exit");
 
             space();
-            selection = selectOption();
+            int selection = selectOption();
 
             switch (selection) {
+                case 2:
+                    carManagement();
                 case 0:
                     System.exit(0);
                     break;
@@ -59,8 +64,48 @@ public class Main {
         }
     }
 
+    private static void carManagement() {
+        boolean systemHasCars = !tempDB.cars.isEmpty();
+        String errorMessage = "";
+
+        while (true) {
+            cleanScreen();
+            System.out.println("<<<CAR-TRIP-PRICE>>>");
+            System.out.println("<<<Car management>>>");
+            System.out.println(errorMessage);
+            space();
+            System.out.println("1) Add new car");
+            if (systemHasCars) {
+                System.out.println("2) Select car");
+                System.out.println("3) Edit current car");
+            }
+            space();
+            System.out.println("0) Go back");
+
+            space();
+            int selection = selectOption();
+
+            if (selection == 1 || selection == 0 || (systemHasCars && (selection == 2 || selection == 3))) {
+                switch (selection) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 0:
+                        mainMenu();
+                        break;
+                }
+            } else {
+                errorMessage = "\"" + selection + ")\" isn't a valid option. Please try again.";
+            }
+        }
+    }
+
     public static void main(String[] args) {
         cleanScreen();
+        tempDB = TempDB.getInstance();
         mainMenu();
     }
 }
