f951098a1aaf66d1c9ff723379a6fea95baa3523
Federico De Luca <federicoNdeluca@gmail.com>
2020-08-27 16:09:55 -0300

    Updated car categories with real data

--- Code/CarTripCost/src/main/java/domain/CarCategory.java
+++ Code/CarTripCost/src/main/java/domain/CarCategory.java
@@ -6,9 +6,13 @@ package domain;
  */
 public enum CarCategory {
 
-    CAT_01("Car category 01"),
-    CAT_02("Car category 03"),
-    CAT_03("Car category 03");
+    CAT_01("Cars, trucks (up to 8 seats, including driver) and other 2-axle vehicles without dual wheels, with a single-axle trailer."),
+    CAT_02("Express buses (driver and one companion maximum), Micros, mini buses and tractor without semi-trailer."),
+    CAT_03("2-axle vehicles with more than 4 wheels."),
+    CAT_04("Bus with passengers"),
+    CAT_05("Vehicles or cargo equipment with 3 axles."),
+    CAT_06("4-axle vehicles or equipment without dual wheels."),
+    CAT_07("Vehicles or cargo equipment with 4 or more axles with dual wheels");
 
     public final String description;
 
